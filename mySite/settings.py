4c4dc94327ad9611d60d1aed5c1a5b19019d4673
Branden <brandenconnected@gmail.com>
2023-09-24 20:05:52 +0200

    attempt 4 ...

--- mySite/settings.py
+++ mySite/settings.py
@@ -49,7 +49,7 @@ CSRF_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = False
 
 CORS_ALLOW_ALL_ORIGINS = True
-CSRF_TRUSTED_ORIGINS = ['www.wazaware.co.za']
+CSRF_TRUSTED_ORIGINS = ['wazaware.co.za', 'www.wazaware.co.za', '162.19.226.81', '127.0.0.1:5000']
 
 ROOT_URLCONF = 'mySite.urls'
 
